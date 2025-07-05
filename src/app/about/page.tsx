6acbef9c4caa4e3236c313341ae36953ccb1abb4
irfan <irfandahusnisahid@gmail.com>
2025-07-06 01:00:20 +0700

    Create homepage banner and portfolio cards, aboutpage, contact page, and navbar

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl">About Page</h1>
+      </div>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,106 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="min-h-[150vh] bg-black flex flex-col items-center text-center pt-20">
+      <h1 className="text-9xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#2000FF] to-[#0596D5] motion-preset-blur-right motion-duration-1000">
+        SOAR
+      </h1>
+      <p className="mt-4 text-lg text-gray-500 motion-preset-blur-right motion-duration-1000">
+        A work gallery by Irfanda Sahid
+      </p>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className="mt-20 px-6 w-full max-w-6xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-2">
+
+      {/* Card 1 */}
+      <a
+        href="/portfolio/project-1"
+        className="relative rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 group h-96"
+      >
+        <img
+          src="/images/minimalism.jpg"
+          alt="Project 1"
+          className="w-full h-full object-cover absolute inset-0 z-0"
+        />
+        <div className="relative z-10 h-full w-full p-4 flex flex-col justify-end items-start text-left bg-gradient-to-t from-black/70 via-black/30 to-transparent">
+          <h3 className="text-xl text-white-300 font-semibold">Medium Articles</h3>
+          <h2 className="text-white text-sm group-hover:text-blue-400 transition">
+            Curated insights and articles based on experiences
+          </h2>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </a>
+
+      {/* Card 2 */}
+      <a
+        href="/portfolio/project-1"
+        className="relative rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 group h-96"
+      >
+        <img
+          src="/images/artsy.jpg"
+          alt="Project 2"
+          className="w-full h-full object-cover absolute inset-0 z-0"
+        />
+        <div className="relative z-10 h-full w-full p-4 flex flex-col justify-end items-start text-left bg-gradient-to-t from-black/70 via-black/30 to-transparent">
+          <h3 className="text-xl text-white-300 font-semibold">Tableau Public</h3>
+          <h2 className="text-white text-sm group-hover:text-blue-400 transition">
+            Aiming to engage the visual senses
+          </h2>
+        </div>
+      </a>
+
+      {/* Card 3 */}
+      <a
+        href="/portfolio/project-1"
+        className="relative rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 group h-96"
+      >
+        <img
+          src="/images/risk.jpg"
+          alt="Project 1"
+          className="w-full h-full object-cover absolute inset-0 z-0"
+        />
+        <div className="relative z-10 h-full w-full p-4 flex flex-col justify-end items-start text-left bg-gradient-to-t from-black/70 via-black/30 to-transparent">
+          <h3 className="text-xl text-white-300 font-semibold">Risk Modeling</h3>
+          <h2 className="text-white text-sm group-hover:text-blue-400 transition">
+            Demonstrating risk modeling techniques
+          </h2>
+        </div>
+      </a>
+
+      {/* Card 4 */}
+            <a
+        href="/portfolio/project-1"
+        className="relative rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 group h-96"
+      >
+        <img
+          src="/images/riskanalyst.jpg"
+          alt="Project 1"
+          className="w-full h-full object-cover absolute inset-0 z-0"
+        />
+        <div className="relative z-10 h-full w-full p-4 flex flex-col justify-end items-start text-left bg-gradient-to-t from-black/70 via-black/30 to-transparent">
+          <h3 className="text-xl text-white-300 font-semibold">Risk Analyst</h3>
+          <h2 className="text-white text-sm group-hover:text-blue-400 transition">
+            Showcasing risk analysis techniques
+          </h2>
+        </div>
+      </a>
+
+      {/* Card 5 */}
+      <a
+        href="/portfolio/project-1"
+        className="relative rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 group h-96"
+      >
+        <img
+          src="/images/agentic_ai.jpg"
+          alt="Project 1"
+          className="w-full h-full object-cover absolute inset-0 z-0"
+        />
+        <div className="relative z-10 h-full w-full p-4 flex flex-col justify-end items-start text-left bg-gradient-to-t from-black/70 via-black/30 to-transparent">
+          <h3 className="text-xl text-white-300 font-semibold">Agentic AI</h3>
+          <h2 className="text-white text-sm group-hover:text-blue-400 transition">
+            Catching up with the latest in AI development
+          </h2>
+        </div>
+      </a>
+</div>
+
+    </main>
   );
 }
